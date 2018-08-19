5bb0dddf51ca40cbcfe12bc506dfaa49590edfc9
AlenaYashchanka <lena1509y@yandex.ru>
2018-08-20 01:24:06 +0200

    try WebdriverIo

--- TEST AUTOMATION/webdriver/banana_test.js
+++ TEST AUTOMATION/webdriver/banana_test.js
@@ -18,11 +18,11 @@ driver.get('http://google.com');
 driver.findElement(By.css('#lst-ib')).sendKeys('Banana Song').then(function(){                        //write "Banana song" in input search
     driver.wait(until.elementLocated(By.css('#sbse0 > div.sbqs_c')), 10000);                           //wait until list appears
     driver.findElement(By.css('#sbse0 > div.sbqs_c')).click().then(function(){                        //click on the first item
-        driver.wait(until.elementLocated(By.name('btnK')), 5000);                                     
+        driver.wait(until.elementIsVisible(By.name('btnK')), 5000);                                     
         driver.findElement(By.name('btnK')).click().then(function(){                                  //click the button "Поиск в google"            
-            driver.executeScript("window.scrollBy(0,100px)");
-            driver.wait(until.elementIsVisible(By.css("a[href*='youtube.com']") && By.partialLinkText('Despicable Me')),5000);
-            driver.findElement(By.css("a[href*='youtube.com']") && By.partialLinkText('Despicable Me')).click();
+            driver.executeScript("window.scrollBy(0,100px)");                                             //try to scroll down the window
+            driver.wait(until.elementIsVisible(By.css("a[href*='youtube.com']") && By.partialLinkText('Despicable Me')),5000);     //wail until link visible
+            driver.findElement(By.css("a[href*='youtube.com']") && By.partialLinkText('Despicable Me')).click();                   //click the link youtube that contains text "Despicable Me"
             });
     });
 });                              
